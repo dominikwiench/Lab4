6f1ab652a0809c407c3722fae8db05b85e643a60
Dominik Wiench <hakerzysko@gmail.com>
2024-07-01 22:12:10 +0200

    add --logs function

--- skrypt.sh
+++ skrypt.sh
@@ -3,3 +3,12 @@
 if [ "$1" == "--date" ]; then
     date
 fi
+
+if [ "$1" == "--logs" ]; then
+    count=${2:-100}
+
+    for ((i=1; i<=count; i++)); do
+        filename="log${i}.txt"
+        echo "Nazwa pliku: $filename, nazwa skryptu: $0, Data: $(date)" > $filename
+    done
+fi
